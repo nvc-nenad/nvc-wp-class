def64b1dcfcdf2d075defedcebfe0efb593279ad
Nenad Manoilov <nmanoilov@alamo.edu>
2018-04-22 23:13:59 -0500

    Added version to the style.css to avoid caching and need to clear cache to see styles changes

--- wp-content/themes/class-tutorial/functions.php
+++ wp-content/themes/class-tutorial/functions.php
@@ -120,7 +120,7 @@ add_action( 'widgets_init', 'class_tutorial_widgets_init' );
  * Enqueue scripts and styles.
  */
 function class_tutorial_scripts() {
-	wp_enqueue_style( 'class-tutorial-style', get_stylesheet_uri() );
+	wp_enqueue_style( 'class-tutorial-style', get_stylesheet_uri().'?v='.time() );
 
 	wp_enqueue_script( 'class-tutorial-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20151215', true );
 
