c5e50760510037649ff4fb0a2297d7f17ed21bc4
Nenad Manoilov <nmanoilov@alamo.edu>
2018-05-09 11:21:27 -0500

    including images folder

--- wp-content/plugins/nvc_wp_basic_plugin/nvc_wp_basic_plugin.php
+++ wp-content/plugins/nvc_wp_basic_plugin/nvc_wp_basic_plugin.php
@@ -13,12 +13,12 @@ Domain Path:  /languages
 
 // remove default dashboard meta boxes
 
-function nvcwp_basic_plugin() {
-    remove_meta_box( 'dashboard_primary', 'dashboard', 'normal' );
-    remove_meta_box( 'dashboard_activity', 'dashboard', 'normal' );
-    remove_meta_box( 'dashboard_quick_press', 'dashboard', 'normal' );
-    remove_meta_box( 'dashboard_right_now', 'dashboard', 'normal' );
-    remove_action('welcome_panel', 'wp_welcome_panel');
-}
-
-add_action( 'admin_init', 'nvcwp_basic_plugin' );
+// function nvcwp_basic_plugin() {
+//     remove_meta_box( 'dashboard_primary', 'dashboard', 'normal' );
+//     remove_meta_box( 'dashboard_activity', 'dashboard', 'normal' );
+//     remove_meta_box( 'dashboard_quick_press', 'dashboard', 'normal' );
+//     remove_meta_box( 'dashboard_right_now', 'dashboard', 'normal' );
+//     remove_action('welcome_panel', 'wp_welcome_panel');
+// }
+//
+// add_action( 'admin_init', 'nvcwp_basic_plugin' );
